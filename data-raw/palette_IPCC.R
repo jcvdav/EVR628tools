3196de3f72dfd9f92f52598474ee9620b56f0705
jcvdav <juancarlos.villader@gmail.com>
2025-08-12 16:28:55 -0400

    Unlisting

--- data-raw/palette_IPCC.R
+++ data-raw/palette_IPCC.R
@@ -25,20 +25,20 @@ colors <- dplyr::tibble(
            "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/cryo_div.txt",
            "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/cryo_seq.txt")
 ) |>
-  dplyr::mutate(rgb = purrr::map(links$link,
+  dplyr::mutate(rgb = purrr::map(link,
                                  readr::read_table,
                                  col_names = c("R", "G", "B")),
                 hex = purrr::map(rgb, rgb2hex))
 
 
-temp_div <- colors$hex[colors$name == "temp_div"]
-temp_seq <- colors$hex[colors$name == "temp_seq"]
-prec_div <- colors$hex[colors$name == "prec_div"]
-prec_seq <- colors$hex[colors$name == "prec_seq"]
-wind_div <- colors$hex[colors$name == "wind_div"]
-wind_seq <- colors$hex[colors$name == "wind_seq"]
-cryo_div <- colors$hex[colors$name == "cryo_div"]
-cryo_seq <- colors$hex[colors$name == "cryo_seq"]
+temp_div <- colors$hex[colors$name == "temp_div"][[1]]
+temp_seq <- colors$hex[colors$name == "temp_seq"][[1]]
+prec_div <- colors$hex[colors$name == "prec_div"][[1]]
+prec_seq <- colors$hex[colors$name == "prec_seq"][[1]]
+wind_div <- colors$hex[colors$name == "wind_div"][[1]]
+wind_seq <- colors$hex[colors$name == "wind_seq"][[1]]
+cryo_div <- colors$hex[colors$name == "cryo_div"][[1]]
+cryo_seq <- colors$hex[colors$name == "cryo_seq"][[1]]
 
 usethis::use_data(temp_div,
                   temp_seq,
