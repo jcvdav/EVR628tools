9644445b4a8f5474296484848c1d608e701f0db0
jcvdav <juancarlos.villader@gmail.com>
2025-08-12 16:16:03 -0400

    Updated IPCC color palette

---
+++ R/palette_IPCC.R
@@ -0,0 +1,37 @@
+#' IPCC color palettes
+#' @description
+#' Provides standard divergent and sequential IPCC color palettes for temperature, precipitation, wind and cryosphere
+#'
+#' @param var Character string indicating the type of variable. Options are "temp", "prec", "wind", "cryo"
+#' @param type Character string specifying if the user wants divergent ("div") or sqeuential "seq" palettes
+#'
+#' @returns A character vector containing hex color codes
+#' @export
+#'
+#' @source Page 8 of the 2019 ICPP Style guide: \url{https://www.ipcc.ch/site/assets/uploads/2019/04/IPCC-visual-style-guide.pdf}
+#' @source Mark Harrower & Cynthia A. Brewer (2003) ColorBrewer.org: An Online Tool for Selecting Colour Schemes forMaps, The Cartographic Journal, 40:1, 27-37, DOI: 10.1179/000870403235002042
+#'
+
+palette_IPCC <- function(var = "temp", type = "div"){
+
+  if(!var %in% c("temp", "prec", "wind", "cryo")) {
+    stop('The value of `var` must be one of: "temp", "prec", "wind", or "cryo"')
+  }
+
+  if(type %in% c("div", "seq")) {
+    stop('The value of `type` must be one of: "div", or "seq"')
+  }
+
+  load("R/sysdata.rda")
+
+  colors <- dplyr::case_when(var == "temp" & type == "div" ~ temp_div,
+                             var == "temp" & type == "seq" ~ temp_seq,
+                             var == "prec" & type == "div" ~ prec_div,
+                             var == "prec" & type == "seq" ~ prec_seq,
+                             var == "wind" & type == "div" ~ wind_div,
+                             var == "wind" & type == "seq" ~ wind_seq,
+                             var == "cryo" & type == "div" ~ cryo_div,
+                             var == "cryo" & type == "seq" ~ cryo_seq)
+
+  return(colors)
+}
---
+++ data-raw/palette_IPCC.R
@@ -0,0 +1,51 @@
+## code to prepare `palette_IPCC_temp_seq` dataset goes here
+
+rgb2hex <- function(vals){
+  grDevices::rgb(red = vals$R,
+                 green = vals$G,
+                 blue = vals$B,
+                 maxColorValue = 256)
+}
+
+colors <- dplyr::tibble(
+  name = c("temp_div",
+           "temp_seq",
+           "prec_div",
+           "prec_seq",
+           "wind_div",
+           "wind_seq",
+           "cryo_div",
+           "cryo_seq"),
+  link = c("https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/temp_div.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/temp_seq.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/prec_div.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/prec_seq.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/wind_div.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/wind_seq.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/cryo_div.txt",
+           "https://raw.githubusercontent.com/IPCC-WG1/colormaps/refs/heads/master/continuous_colormaps_rgb_0-255/cryo_seq.txt")
+) |>
+  dplyr::mutate(rgb = purrr::map(links$link,
+                                 readr::read_table,
+                                 col_names = c("R", "G", "B")),
+                hex = purrr::map(rgb, rgb2hex))
+
+
+temp_div <- colors$hex[colors$name == "temp_div"]
+temp_seq <- colors$hex[colors$name == "temp_seq"]
+prec_div <- colors$hex[colors$name == "prec_div"]
+prec_seq <- colors$hex[colors$name == "prec_seq"]
+wind_div <- colors$hex[colors$name == "wind_div"]
+wind_seq <- colors$hex[colors$name == "wind_seq"]
+cryo_div <- colors$hex[colors$name == "cryo_div"]
+cryo_seq <- colors$hex[colors$name == "cryo_seq"]
+
+usethis::use_data(temp_div,
+                  temp_seq,
+                  prec_div,
+                  prec_seq,
+                  wind_div,
+                  wind_seq,
+                  cryo_div,
+                  cryo_seq,
+                  internal = TRUE, overwrite = TRUE)
