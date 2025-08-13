a70b5b6ab2db9b15583b297ab7b9066f7b916339
jcvdav <juancarlos.villader@gmail.com>
2025-08-13 10:24:38 -0400

    Added one column to lionfish

--- data-raw/data_lionfish.R
+++ data-raw/data_lionfish.R
@@ -13,12 +13,16 @@
 # Data come from Alisha's repository at https://github.com/jcvdav/LionfishHabitat
 # Read, select columns
 data_lionfish <- readr::read_csv("https://raw.githubusercontent.com/jcvdav/LionfishHabitat/refs/heads/master/data/lionfishdata.csv") |>
+  dplyr::mutate(size_class = dplyr::case_when(total_length < 100 ~ "small",
+                                              dplyr::between(total_length, 100, 200) ~ "medium",
+                                              total_length > 200 ~ "large")) |>
   dplyr::select(id,
                 site = location,
                 lat = latitude,
                 lon = longitude,
                 total_length_mm = total_length,
                 total_weight_gr = total_weight,
+                size_class,
                 depth_m = depth,
                 temperature_C = temperature)
 
