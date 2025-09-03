b5251f82d0bb425b5a7f72ae54b9b22ab141d4ef
jcvdav <juancarlos.villader@gmail.com>
2025-09-03 15:56:29 -0400

    Added new data set

--- R/data.R
+++ R/data.R
@@ -114,7 +114,7 @@
 #' lat_max = 25.75) for 2024, relative to 2000-2023 values.
 #'
 #' @format ## `data_sst_anom`
-#' A data.frame with 9 rowns and two columns:
+#' A data.frame with 9 rows and two columns:
 #' \describe{
 #'   \item{lat}{Numeric - Latitude in decimal degrees indicating the center of the pixel}
 #'   \item{lon}{Numeric - Longitude in decimal degrees indicating the center of the pixel}
@@ -128,6 +128,30 @@
 #' # Look at the first 10 rows
 #' head(EVR628tools::data_sst_anom, 10)
 "data_sst_anom"
+
+#' Marine Heatwaves in TURF-managed fisheries
+#'
+#' @description
+#' Time series (1982-2021) of Marine Heatwaves identified for 55 fisheries along
+#' the Baja  California Peninsula.
+#'
+#' @format ## `data_heatwaves`
+#' A data.frame with 220 rows and 8 columns:
+#' \describe{
+#'    \item{fishery}{Character - Specifies the fishery (lobster, sea_cucumber, or urchin)}
+#'    \item{eu_rnpa}{Character - A 10-digit unique identifier for each economic unit}
+#'    \item{year}{Numeric - Year in which the marine heatwave occurred}
+#'    \item{temp_mean}{Numeric - Mean sea surface temperature within a TURF (°C)}
+#'    \item{mhw_events}{Numeric - Number of marine heatwave events}
+#'    \item{mhw_days}{Numeric - Total number of days under marine heatwave conditons}
+#'    \item{mhw_int_mean}{Numeric - Mean marine heatwave intensity, defined as degrees in excess of the climatological threshold (°C)}
+#'    \item{mhw_int_cummulative}{Numeric - Cumulative marine heatwave intensity, measured in °C days}
+#' }
+#'
+#' @source Villaseñor-Derbez, J.C., Arafeh-Dalmau, N. & Micheli, F. Past and future
+#' impacts of marine heatwaves on small-scale fisheries in Baja California, Mexico.
+#' Commun Earth Environ 5, 623 (2024). \url{https://doi.org/10.1038/s43247-024-01696-x}
+"data_heatwaves"
 
 #"scuba"
 
---
+++ data-raw/data_heatwaves.R
@@ -0,0 +1,17 @@
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# jc_villasenor@miami.edu
+# date
+#
+# code to prepare `data_heatwaves` dataset goes here
+#
+################################################################################
+#
+# Data come from https://github.com/jcvdav/ssf_shocks/raw/refs/heads/main/data/processed/annual_environmental_panel.rds
+
+data_heatwaves <- readRDS(url("https://github.com/jcvdav/ssf_shocks/raw/refs/heads/main/data/processed/annual_environmental_panel.rds")) |>
+  dplyr::select(fishery, eu_rnpa, year, temp_mean, mhw_events, mhw_days, mhw_int_mean, mhw_int_cumulative)
+
+# Export
+usethis::use_data(data_heatwaves, overwrite = TRUE)
